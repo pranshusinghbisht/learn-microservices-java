fe90856ef33fd3783e32b38719f0b30b57f7730c
pranshu.bisht <pranshu.bisht@tc-pl.in>
2024-05-16 15:54:54 +0530

    add

--- UserService/src/main/java/com/user/service/userservice/UserServiceApplication.java
+++ UserService/src/main/java/com/user/service/userservice/UserServiceApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserServiceApplication {
 
+//	@Bean
+//	public RestTemplate restTemplate(){
+//		return new RestTemplate();
+//	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
--- UserService/src/main/java/com/user/service/userservice/services/UserServiceImpl.java
+++ UserService/src/main/java/com/user/service/userservice/services/UserServiceImpl.java
@@ -1,19 +1,32 @@
 package com.user.service.userservice.services;
 
+import com.user.service.userservice.entities.Hotel;
+import com.user.service.userservice.entities.Rating;
 import com.user.service.userservice.entities.User;
 import com.user.service.userservice.exceptions.ResourceNotFoundException;
 import com.user.service.userservice.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.client.RestTemplate;
+import com.google.gson.Gson; // Include Gson library
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate = new RestTemplate();
+
+    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Override
     public User saveUser(User user) {
@@ -29,6 +42,40 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUserById(String userId) {
-        return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id is not found on server"));
+
+        // get user from database with the help of user repository
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id is not found on server"));
+        // i want rating also then
+        // fetch rating of the above user from RATING SERVICE
+
+        // http://localhost:8083/ratings/users/50a552cd-90f3-4d3a-8164-3872ae315d5e
+        ArrayList<Rating> ratings = restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserId(), ArrayList.class);
+        // in prod localhost and port might change it should come with dynamic
+
+        // Log the full ratings object using Gson
+        Gson gson = new Gson();
+        String jsonRatings = gson.toJson(ratings);
+        logger.info("Ratings fetched for user: {}", jsonRatings);
+
+
+        List<Rating> ratingList = ratings.stream().map(rating -> {
+            // api call to hotel service to get the hotel service
+
+
+           ResponseEntity<Hotel> forEntity = restTemplate.getForEntity("http://localhost:8082/hotels/"+rating.getHotelId(), Hotel.class);
+
+            Hotel hotel = forEntity.getBody();
+            logger.info("Hotel fetched for user: {}", hotel);
+
+            // set the hotel to rating
+
+            rating.setHotel(hotel);
+
+            // return the rating
+            return rating;
+        }).collect(Collectors.toList());
+
+        user.setRatings(ratingList);
+        return user;
     }
 }
